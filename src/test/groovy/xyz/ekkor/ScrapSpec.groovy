3888414fc49fe5fcf983dca24e6d7cd76688f638
eunsebi Mac <eunsebi@me.com>
2019-06-11 17:57:52 +0900

    commit

---
+++ src/test/groovy/xyz/ekkor/ScrapSpec.groovy
@@ -0,0 +1,18 @@
+package xyz.ekkor
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class ScrapSpec extends Specification implements DomainUnitTest<Scrap> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
